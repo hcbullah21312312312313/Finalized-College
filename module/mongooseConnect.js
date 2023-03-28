c5f5f70a1a5f8b411ce215a189b523d880a61ed4
Haseeb Ullah Tarakai <haseebullah7aand7b@gmail.com>
2023-03-28 16:38:24 +0500

    Bug resolved

--- module/mongooseConnect.js
+++ module/mongooseConnect.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/college', {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>
+mongoose.connect('mongodb+srv://admin:p5W5Rvr28zzQAhWE@cluster0.bp2y7j1.mongodb.net/College', {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>
 console.log('Connected to the database'))
 const StudentsSchema=new mongoose.Schema({
     fname:String,
